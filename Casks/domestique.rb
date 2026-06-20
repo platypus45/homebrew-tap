c2478dc4db4a7757348263f5f64c21a9de6d4285
platypus45 <86323611+platypus45@users.noreply.github.com>
2026-06-20 17:36:41 +0200

    domestique 2.2.2

--- Casks/domestique.rb
+++ Casks/domestique.rb
@@ -20,8 +20,8 @@
 #     shasum -a 256 /tmp/D.dmg
 #     # Update version + sha256 below, then commit + push to the tap repo.
 cask "domestique" do
-  version "2.2.1"
-  sha256 "5876fbdf4cefaa257dab99d93d19386c8f0e98d1323a9e49bb6b756cef5cfe0e"
+  version "2.2.2"
+  sha256 "f41e152227c15bccde2697227028ef4b351f9dea4a0d8adc674eb6063d140d89"
 
   url "https://github.com/platypus45/domestique/releases/download/v#{version}/Domestique-v#{version}.dmg"
   name "Domestique"
