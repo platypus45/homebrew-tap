624c3ff5ce236bb2aa5b482b2cd075756ddeaa6c
platypus45 <info@praktijkpiloot.nl>
2026-07-03 16:55:12 +0200

    domestique 3.0.2

--- Casks/domestique.rb
+++ Casks/domestique.rb
@@ -20,8 +20,8 @@
 #     shasum -a 256 /tmp/D.dmg
 #     # Update version + sha256 below, then commit + push to the tap repo.
 cask "domestique" do
-  version "3.0.1"
-  sha256 "57f060bc468d80c7eee9d8c74bb45876df00e006a88e157d84f13597aa957113"
+  version "3.0.2"
+  sha256 "056ea81c75161a2a5390d2abcd416ea7a6168201f54e0177ad7bce307a6983a1"
 
   url "https://github.com/platypus45/domestique/releases/download/v#{version}/Domestique-v#{version}.dmg"
   name "Domestique"
